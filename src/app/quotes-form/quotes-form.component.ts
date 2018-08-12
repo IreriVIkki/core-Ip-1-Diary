697d687ca546a1e4e64ef0e0386daeb5607d26db
IreriVikki <wambsviki@gmail.com>
2018-08-13 01:01:36 +0300

    added some styling to the header

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -17,7 +17,26 @@ export class QuotesFormComponent implements OnInit {
 
   ngOnInit() {
     this.newQuote = new Quotes(0, "", 0, 0, "", "", new Date());
-    this.quotes = [];
+    this.quotes = [
+      new Quotes(
+        0,
+        "It took thirty-eight years before 50 million people gained access to radios. It took television thirteen years to earn an audience that size. It took Instagram a year and a half.",
+        0,
+        0,
+        "Gary Vaynerchuk",
+        "Victor Ireri",
+        new Date()
+      ),
+      new Quotes(
+        0,
+        "life shrinks and expands on the proportion of your willingness to take risks and try new things.",
+        0,
+        0,
+        "Gary Vaynerchuk",
+        "Victor Ireri",
+        new Date()
+      )
+    ];
   }
 
   @Output()
