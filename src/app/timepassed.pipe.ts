0cc868b14ae788a6803b3fc259474d5dd38a2ea2
IreriVikki <wambsviki@gmail.com>
2018-08-12 15:35:08 +0300

    Added days hours minutes and seconds to the timepasses pipe

--- src/app/timepassed.pipe.ts
+++ src/app/timepassed.pipe.ts
@@ -4,29 +4,30 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "timepassed"
 })
 export class TimepassedPipe implements PipeTransform {
-  transform(value: any): number {
-    let today: Date = new Date(); //get current date and time
+  transform(value: any): any {
+    let today: any = new Date(); //get current date and time
     let todayWithNoTime: any = new Date(
       today.getFullYear(),
       today.getMonth(),
       today.getDate()
     );
-    var dateDifference = Math.abs(value - todayWithNoTime); // returns value in milliseconds
-    const secondsInADay = 86400; //60 seconds*60 minutes in an hour *24 hours
+    var dateDifference = Math.abs(value - today); // returns value in milliseconds
 
-    var dateDifferenceSeconds = dateDifference * 0.001; //converts to seconds
+    var Seconds = dateDifference * 0.001; //converts to seconds
 
-    var dateCounter = dateDifferenceSeconds / secondsInADay;
+    var days = parseInt(Seconds / 86400);
+    var hours = parseInt((Seconds % 86400) / 3600);
+    var minutes = parseInt(((Seconds % 86400) % 3600) / 60);
+    var seconds = parseInt(Seconds);
 
-    // this.days = parseInt(time / 86400);
-    // this.hours = parseInt((time % 86400) / 3600);
-    // this.minutes = parseInt(((time % 86400) % 3600) / 60);
-    // this.seconds = parseInt(((100000 % 86400) % 3600) % 60);
-
-    if (dateCounter >= 86400) {
-      return dateCounter;
+    if (Seconds >= 86400) {
+      return days + " days";
+    } else if (Seconds < 86400 && Seconds >= 3600) {
+      return hours + " hours ";
+    } else if (Seconds < 3600 && Seconds >= 60) {
+      return minutes + " minutes ";
     } else {
-      return 0;
+      return seconds + " seconds ";
     }
   }
 }
